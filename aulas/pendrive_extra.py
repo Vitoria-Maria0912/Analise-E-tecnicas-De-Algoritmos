9078db1b150ce61b6978ae2af57c277b0b52b9e1
Vitória Maria do Nascimento <116987726+Vitoria-Maria0912@users.noreply.github.com>
2025-08-20 20:22:17 +0000

    feat: Questão extra do pendrive.

---
+++ aulas/pendrive_extra.py
@@ -0,0 +1,21 @@
+def pendrive(n, c, t, out, parcial, i, soma):
+    if(i >= n): 
+        if(soma == c): out += [parcial]
+    else:
+        soma = sum(parcial)
+        if(soma < c): 
+            out = pendrive(n, c, t, out, parcial+[t[i]], i+1, soma)
+        out = pendrive(n, c, t, out, parcial, i+1, soma)
+    return out
+
+# Testes
+ns = [8]
+cs = [90]
+ts = [[10, 15, 20, 20, 30, 35, 40, 50]]
+esperado = [4]
+
+for i, n, c, t, out in zip(range(8), ns, cs, ts, esperado):
+    t = sorted(t, reverse=True)
+    resposta = pendrive(n, c, t, [], [], 0, 0)
+    resposta_final = len(max(resposta, key=len))
+    assert resposta_final == out, f"Erro {i}: esperado:{out}, tamanho:{resposta_final}, conjunto:{resposta}"
